cb38b4e3e5356f34b5f98e99b5312585fa48bd3a
Abhi70815 <abhishekchaurasia45621@gmail.com>
2025-03-07 22:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+
+let data={name:'',year:''};
+let url ='https://localhost:8080/'
+let urlNode ;
+
+window.onload=function(){
+	 urlNode = document.getElementById('url');
+urlNode.innerText=url;
+}
+
+function handleInput(e){
+	let {name,value}=e.target;
+	data[name]=value;
+	
+}
+
+function handleUrl(e){
+	e.preventDefault()
+  let queryString = `?name=${data.name}&year=${data.year}`;
+            let newUrl = url + queryString; // Combine base URL with query string
+
+            urlNode.innerText = newUrl; 
+}
